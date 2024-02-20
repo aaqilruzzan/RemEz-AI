e5979b73b8a231cc2fc5ae07c3b0add07d6e0af6
anudaj <anudamcg@gmail.com>
2024-02-20 18:40:06 +0530

    Did the calling the LLM with questions and retrieving answers part

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from transformers import AutoTokenizer, AutoModel
 import faiss
 import torch
 
+from langchain.llms import huggingface_hub
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+
 def get_pdf_text(pdf_docs):
     text = ""   
     for pdf in pdf_docs:
@@ -45,13 +50,40 @@ def create_faiss_index(embeddings):
         faiss_index.add(embedding)
     return faiss_index
 
+def get_conversation_chain(vectorstore):
+    llm = huggingface_hub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm( 
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain 
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("$MSG", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("$MSG}", message.content), unsafe_allow_html=True)
+
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="PDF Q&A")
-    
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None    
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
     st.header("PDF Question Generator")
-    st.text_input("Ask a question about your documents:") 
+    user_question = st.text_input("Ask a question about your documents:") 
+    if user_question:
+        handle_userinput(user_question) 
 
     with st.sidebar:
         st.subheader("Your Documents")
@@ -66,7 +98,8 @@ def main():
                 embeddings = get_embeddings(text_chunks)
                 faiss_index = create_faiss_index(embeddings)
 
-                st.success("Documents processed and embeddings stored!")
+                st.session_state.conversation = get_conversation_chain(faiss_index)
+                st.success("Documents RAG Process done!")
 
 if __name__ == '__main__':
     main()
