43cb49e02f0d7c59febb34d902b1a20cb017ecb5
anudaj <anudamcg@gmail.com>
2024-03-09 20:56:01 +0530

    did openai implementations.

--- rag.py
+++ rag.py
@@ -3,10 +3,12 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader    
 from langchain.text_splitter import CharacterTextSplitter
 from transformers import AutoTokenizer, AutoModel
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 import faiss
 import torch
-
-from langchain.llms import huggingface_hub
+from langchain.llms import HuggingFaceHub
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
@@ -30,6 +32,12 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
 def get_embeddings(text_chunks):
     model_name = "distilbert-base-uncased"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -51,25 +59,29 @@ def create_faiss_index(embeddings):
     return faiss_index
 
 def get_conversation_chain(vectorstore):
-    llm = huggingface_hub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm( 
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
         memory=memory
     )
-    return conversation_chain 
+    return conversation_chain
 
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
+
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace("$MSG", message.content), unsafe_allow_html=True)
+            st.write(user_template.replace(
+                "$MSG", message.content), unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace("$MSG}", message.content), unsafe_allow_html=True)
-
-
+            st.write(bot_template.replace(
+                "$MSG", message.content), unsafe_allow_html=True)
 
 def main():
     load_dotenv()
@@ -95,10 +107,10 @@ def main():
                 
                 text_chunks = get_text_chunks(raw_text)
 
-                embeddings = get_embeddings(text_chunks)
-                faiss_index = create_faiss_index(embeddings)
+                vectorstore = get_vectorstore(text_chunks)
 
-                st.session_state.conversation = get_conversation_chain(faiss_index)
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
                 st.success("Documents RAG Process done!")
 
 if __name__ == '__main__':
